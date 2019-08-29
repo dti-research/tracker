e7f10f49fda86e14a5508ecdf5ab37b5995f0af3
Nicolai Anton Lynnerup <nily@teknologisk.dk>
2019-08-29 13:10:41 +0200

    Added start of git utility script

---
+++ tracker/utils/git.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2019, Danish Technological Institute.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# -*- coding: utf-8 -*-
+
+""" Utility script for handling Git through python
+
+    Depends on `sh` package
+"""
+
+import logging
+# import sh
+
+log = logging.getLogger(__name__)
