1bc24b9e7caa4bd78d5e7981a2d078e45026d572
Nicolai Anton Lynnerup <nily@teknologisk.dk>
2019-10-07 14:01:01 +0200

    Added diff file with raw cmds

---
+++ tracker/commands/diff.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2019, Danish Technological Institute.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# -*- coding: utf-8 -*-
+
+""" Command to get diff between two or three experiments or trials
+"""
+
+# diff -ru [FILE1] [FILE2]
+# colordiff -ru [FILE1] [FILE2]
+# meld [FILE1] [FILE2] [FILE3]
